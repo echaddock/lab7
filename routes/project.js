a1bb4452201f6b0bd0952b19689a1678206d4c1e
vagrant <vagrant@precise32.(none)>
2014-02-20 18:28:04 +0000

    added pugs

--- routes/project.js
+++ routes/project.js
@@ -5,6 +5,9 @@ exports.projectInfo = function(req, res) { 
 
   // query for the specific project and
   // call the following callback
+  models.Project
+     .find({ "_id": projectID})
+     .exec(afterQuery);
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -17,12 +20,36 @@ exports.addProject = function(req, res) {
   console.log(form_data);
 
   // make a new Project and save it to the DB
-  // YOU MUST send an OK response w/ res.send();
-}
+  // YOU MUST send an OK response w/ res.send
+  console.log(form_data);
+  var newPost = new models.Project({ 
+    "title": form_data['title'],
+    "date" : form_data['date'],
+    "summary": form_data['summary'],
+    "image": form_data['image_url']
+
+  });
+  newPost.save(addCallback);
+
+  function addCallback(err) { // this is a callback
+    if(err) {console.log(err);}
+    res.send();
+   }
+
+  }
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
 
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
+  models.Project
+    .find({"_id": projectID})
+    .remove()
+    .exec(deleteCallback);
+
+    function deleteCallback(err) {
+    if(err) { console.log(err); }
+    res.send();
+     }
 }
